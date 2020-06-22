c6e2d196f66ab9b22e5546c577075372b388f6bc
patrikhuber <patrihub@icloud.com>
2020-06-22 16:21:03 +0200

    Removed unused using.

--- FarmerzonAddress/Controllers/CityController.cs
+++ FarmerzonAddress/Controllers/CityController.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using FarmerzonAddressManager.Implementation;
 using FarmerzonAddressManager.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;

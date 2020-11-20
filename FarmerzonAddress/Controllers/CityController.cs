4b954bee81effd529a6c2bce1a7b606fabbe4b5c
patrikhuber <pahub@icloud.com>
2020-11-20 19:41:14 +0100

    Removed unused imports

--- FarmerzonAddress/Controllers/CityController.cs
+++ FarmerzonAddress/Controllers/CityController.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Dapr;
 using FarmerzonAddressManager.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
